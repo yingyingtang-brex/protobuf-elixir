af486dab59a3c5304fb4563b2a790438e05c81a3
yingyingtang-brex <61437239+yingyingtang-brex@users.noreply.github.com>
2020-11-04 15:32:10 -0800

    remove lowercase and add full_name func only when custom_field_options=true

--- test/protobuf/protoc/proto_gen/extension2.pb.ex
+++ test/protobuf/protoc/proto_gen/extension2.pb.ex
@@ -27,7 +27,7 @@ defmodule Ext.Nested do
   defstruct [:my_timestamp]
 
   def full_name do
-    "ext.nested"
+    "ext.Nested"
   end
 
   oneof :my_timestamp, 0
@@ -84,7 +84,7 @@ defmodule Ext.MyMessage do
   ]
 
   def full_name do
-    "ext.mymessage"
+    "ext.MyMessage"
   end
 
   oneof :enums_oneof, 0
